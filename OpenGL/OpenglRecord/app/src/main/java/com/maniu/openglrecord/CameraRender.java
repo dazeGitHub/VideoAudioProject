ff36b877c1a1ea52813cd796b53ecd08d7cd0728
zyz <1992330573@qq.com>
2024-01-05 19:16:51 +0800

    OpenglRecord

---
+++ OpenGL/OpenglRecord/app/src/main/java/com/maniu/openglrecord/CameraRender.java
@@ -0,0 +1,59 @@
+package com.maniu.openglrecord;
+
+import android.graphics.SurfaceTexture;
+import android.opengl.GLSurfaceView;
+
+import androidx.camera.core.Preview;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.maniu.openglrecord.utils.CameraHelper;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class CameraRender implements GLSurfaceView.Renderer , Preview.OnPreviewOutputUpdateListener, SurfaceTexture.OnFrameAvailableListener {
+    private CameraView cameraView;
+    private CameraHelper cameraHelper;
+    private SurfaceTexture mCameraTexure;
+    private  int[] textures;
+    float[] mtx = new float[16];
+    private CameraFilter cameraFilter;
+    public CameraRender(CameraView cameraView) {
+        this.cameraView = cameraView;
+        LifecycleOwner lifecycleOwner = (LifecycleOwner) cameraView.getContext();
+//        打开摄像头
+        cameraHelper = new CameraHelper(lifecycleOwner, this);
+
+
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        textures = new int[1];
+        mCameraTexure.attachToGLContext(textures[0]);
+        mCameraTexure.setOnFrameAvailableListener(this);
+        cameraFilter = new CameraFilter(cameraView.getContext());
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        cameraFilter.setSize(width,height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        mCameraTexure.updateTexImage();
+        mCameraTexure.getTransformMatrix(mtx);
+        cameraFilter.setTransformMatrix(mtx);
+    }
+    //当有数据 过来的时候   onFrameAvailable 一次
+    @Override
+    public void onFrameAvailable(SurfaceTexture surfaceTexture) {
+        cameraView.requestRender();
+    }
+
+    @Override
+    public void onUpdated(Preview.PreviewOutput output) {
+        mCameraTexure=output.getSurfaceTexture();
+    }
+}
---
+++ OpenGL/OpenglRecord/app/src/main/java/com/maniu/openglrecord/CameraView.java
@@ -0,0 +1,31 @@
+package com.maniu.openglrecord;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class CameraView extends GLSurfaceView {
+    private  CameraRender renderer;
+    public CameraView(Context context) {
+        super(context);
+    }
+
+    public CameraView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        renderer = new CameraRender(this);
+        setEGLContextClientVersion(2);
+        setRenderer(renderer);
+        /**
+         * 刷新方式：
+         *     RENDERMODE_WHEN_DIRTY 手动刷新，調用requestRender();
+         *     RENDERMODE_CONTINUOUSLY 自動刷新，大概16ms自動回調一次onDrawFrame方法
+         */
+        //注意必须在setRenderer 后面。
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+
+}
