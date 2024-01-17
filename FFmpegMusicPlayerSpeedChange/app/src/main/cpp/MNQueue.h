383445377c7e1ddd589bbd322d79c607f46d6dbe
zyz <1992330573@qq.com>
2024-01-17 20:50:56 +0800

    again

---
+++ FFmpegMusicPlayerSpeedChange/app/src/main/cpp/MNQueue.h
@@ -0,0 +1,45 @@
+//
+// Created by yangw on 2018-3-6.
+//
+
+#ifndef MYMUSIC_WLQUEUE_H
+#define MYMUSIC_WLQUEUE_H
+
+#include "queue"
+#include "pthread.h"
+#include "AndroidLog.h"
+#include "MNPlaystatus.h"
+
+extern "C"
+{
+#include "libavcodec/avcodec.h"
+};
+
+
+class MNQueue {
+
+public:
+    std::queue<AVPacket *> queuePacket;
+    pthread_mutex_t mutexPacket;
+    pthread_cond_t condPacket;
+    MNPlaystatus *playstatus = NULL;
+
+public:
+
+    MNQueue(MNPlaystatus *playstatus);
+    ~MNQueue();
+
+    int putAvpacket(AVPacket *packet);
+    int getAvpacket(AVPacket *packet);
+
+    int getQueueSize();
+
+    void clearAvpacket();
+
+
+
+
+};
+
+
+#endif //MYMUSIC_WLQUEUE_H
---
+++ WangyiMusicPlayerNoise/app/src/main/cpp/MNPlaystatus.cpp
@@ -0,0 +1,9 @@
+//
+// Created by maniu on 2022/8/5.
+//
+
+#include "MNPlaystatus.h"
+
+MNPlaystatus::MNPlaystatus() {
+    exit = false;
+}
